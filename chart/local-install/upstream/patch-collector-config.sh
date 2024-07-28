e9eb37e2694448d31c552e4cd849bbf990e3763d
David Levanon <dvdlevanon@gmail.com>
2024-07-28 16:30:48 +0300

    upstream test finish

--- chart/local-install/upstream/patch-collector-config.sh
+++ chart/local-install/upstream/patch-collector-config.sh
@@ -1,1 +1,2 @@
-kubectl patch opentelemetrycollector miggo-operator -n miggo-space --type=merge --patch-file collector-config.yaml
+# kubectl patch opentelemetrycollector miggo-operator -n miggo-space --type=merge --patch-file collector-config.yaml
+kubectl -n miggo-space apply -f collector-config.yaml
